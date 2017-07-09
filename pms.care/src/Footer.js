2468cae679a5863bf4bee5781d41e62500a60cf6
Sam Lin <slin@lendingclub.com>
2017-07-08 20:52:08 -0700

    Added Footer

---
+++ pms.care/src/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Footer extends Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.cart.length ?
+        <h1>{this.props.cart.length} Items</h1>:""
+        }
+        
+        <button>Submit</button>
+      </div>
+    );
+  }
+}
+
+export default Footer;
