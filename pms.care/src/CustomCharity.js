859d164d4034b3c9319e64a1c3bf92cf203949bb
Sam Lin <slin@lendingclub.com>
2017-07-09 13:12:34 -0700

    added mock charities

--- pms.care/src/CustomCharity.js
+++ pms.care/src/CustomCharity.js
@@ -53,7 +53,8 @@ class CustomCharity extends Component {
       displayCategories: true,
       displayCharities: false,
       selectedCharitiesList: {},
-      selectedCharity: {}
+      selectedCharity: {},
+      charityIsSelected: false
     });
   }
 
